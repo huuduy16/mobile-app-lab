628013f5ff38f558686345ed97999c3d5dc75234
lehuyaa <44548159+huuduy16@users.noreply.github.com>
2021-12-22 23:48:41 +0700

    week-6

---
+++ app/src/main/java/com/example/donation10/Report.java
@@ -0,0 +1,65 @@
+package com.example.donation10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class Report extends AppCompatActivity {
+
+    ListView listView;
+    static final String[] numbers = new String[] {
+            "Amount, Pay method",
+            "10, Direct",
+            "100, PayPal",
+            "1000, Direct",
+            "10, PayPal",
+            "5000, PayPal"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        listView = (ListView) findViewById(R.id.reportList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, numbers);
+        listView.setAdapter(adapter);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        switch (item.getItemId())
+        {
+            case R.id.menuReport:
+                startActivity (new Intent(this, Report.class));
+                break;
+            case R.id.action_settings:
+                Toast toast_setting = Toast.makeText(this, "Setting Selected",
+                        Toast.LENGTH_SHORT);
+                toast_setting.show();
+                break;
+            case R.id.menuDonate:
+                startActivity (new Intent(this, MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
