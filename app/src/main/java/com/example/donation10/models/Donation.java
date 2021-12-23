4aba8cbd47ad377c530e1ae52c30a6be15f65109
lehuyaa <44548159+huuduy16@users.noreply.github.com>
2021-12-23 21:57:29 +0700

    week 10

--- app/src/main/java/com/example/donation10/models/Donation.java
+++ app/src/main/java/com/example/donation10/models/Donation.java
@@ -1,6 +1,7 @@
 package com.example.donation10.models;
 
 public class Donation {
+    public int id;
     public int amount;
     public String method;
     public Donation (int amount, String method)
@@ -8,4 +9,14 @@ public class Donation {
         this.amount = amount;
         this.method = method;
     }
+    public Donation ()
+    {
+        this.amount = 0;
+        this.method = "";
+    }
+    public String toString()
+    {
+        return id + ", " + amount + ", " + method;
+    }
+
 }
